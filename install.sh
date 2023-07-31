60b67a2d79252e3b825e24080d9102de699432f0
Esonhugh <esonhughoutside@gmail.com>
2023-07-31 13:37:50 +0800

    change the install.sh no force install to global

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
 chmod +x gopherus3.py
-sudo ln -sf $(pwd)/gopherus3.py /usr/local/bin/gopherus3
+# sudo ln -sf $(pwd)/gopherus3.py /usr/local/bin/gopherus3
 echo "Gopherus3 installed"
