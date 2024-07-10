f079ee3cd7ef1f30d32cf819c9e392f74b4c5f64
Esonhugh <esonhughoutside@gmail.com>
2024-07-10 15:09:57 +0800

    update: bug fix for 0d0a

--- gopherus3/piper/__init__.py
+++ gopherus3/piper/__init__.py
@@ -1,15 +1,15 @@
 class LineN: 
     def pipe(self, line):
-        return line.replace("%%0d%%0a", '%%0d')
+        return line.replace("%0d%0a", '%0a').replace("%0D%0A", '%0A')
 
 
 class LineRN:
     def pipe(self, line):
-        return line.replace("%%0a", '%%0d%%0a')
+        return line.replace("%0a", '%0d%0a').replace("%0A", '%0D%0A')
     
 class EndWith00:
     def pipe(self, line):
-        return line + '%%00'
+        return line + '%00'
 
 class Default:
     def pipe(self, line):
